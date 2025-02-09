089feda028a3ea9e849de02ce96839ad94c4a836
DonivanHawkins103 <150474492+DonivanHawkins103@users.noreply.github.com>
2025-02-09 17:41:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+addEventListener("DOMContentLoaded", async function() {
+    const response = await this.fetch("https://glitch.com/edit/#!/yellow-frosted-blouse/api/songs")
+    const songs = await response.json()
+
+    let html = ""
+    for (let song of songs)
+        html+=`<li> ${song.title} - ${song.artist}</li>`
+
+    document.querySelector("#addedsong").innerHTML = html
+})
